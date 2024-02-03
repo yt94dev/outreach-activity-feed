0b6b03eba8c517086bf9548d306136b0fc1ff9e1
Yaroslav Taranenko <yaroslavtaranenko@Mac-Studio-Yaroslav.local>
2024-02-03 18:34:01 +0200

    init file structure

---
+++ src/components/ActivityIcon/ActivityIcon.tsx
@@ -0,0 +1,13 @@
+
+import styles from "./ActivityIcon.module.css";
+
+function ActivityIcon() {
+
+    return (
+        <>
+            <div className={styles.icon}></div>
+        </>
+    );
+}
+
+export default ActivityIcon;
