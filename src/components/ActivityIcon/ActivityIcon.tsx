3c007d0e42ad6385e5f2e209f70d18de4ee0b2e0
Yaroslav Taranenko <yaroslavtaranenko@Mac-Studio-Yaroslav.local>
2024-02-06 21:16:55 +0200

    activityIcon component

--- src/components/ActivityIcon/ActivityIcon.tsx
+++ src/components/ActivityIcon/ActivityIcon.tsx
@@ -1,12 +1,53 @@
 
+import { FC, ReactElement } from "react";
+import { clsx } from 'clsx';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import CallIcon from '@mui/icons-material/Call';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import CoffeeIcon from '@mui/icons-material/Coffee';
+import FreeBreakfastIcon from '@mui/icons-material/FreeBreakfast';
+import PersonIcon from '@mui/icons-material/Person';
+
 import styles from "./ActivityIcon.module.css";
 
-function ActivityIcon() {
+interface IActivityIconProps {
+    type: string,
+    hover?: boolean,
+}
+
+enum iActivityTypeEnum {
+    Text = 'text',
+    Message = 'message',
+    Call = 'call',
+    Coffee = 'coffee',
+    Lunch = 'lunch',
+    Meeting = 'meeting',
+}
+
+const ActivityIcon: FC<IActivityIconProps> = ({type, hover}) => {
+    const getIcon = () : ReactElement => {
+        switch (type) {
+            case iActivityTypeEnum.Text:
+                return (<FormatListBulletedIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            case iActivityTypeEnum.Message:
+                return (<ChatBubbleIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            case iActivityTypeEnum.Call:
+                return (<CallIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            case iActivityTypeEnum.Coffee:
+                return (<CoffeeIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            case iActivityTypeEnum.Lunch:
+                return (<FreeBreakfastIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            case iActivityTypeEnum.Meeting:
+                return (<PersonIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+            default:
+                return (<FormatListBulletedIcon className={styles.iconType} sx={{ fontSize: 18 }} />);
+        }
+    };
 
     return (
-        <>
-            <div className={styles.icon}></div>
-        </>
+        <div className={clsx(styles.icon, hover && styles.blue)}>
+            {getIcon()}
+        </div>
     );
 }
 
